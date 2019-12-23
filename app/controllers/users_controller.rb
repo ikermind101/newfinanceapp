54fb4c314a6753710d240ffc828259dde6eecf27
ikermind101 <isaac.m.ahern@gmail.com>
2019-12-23 10:04:37 -0500

    add friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,10 +14,23 @@ class UsersController < ApplicationController
       flash.now[:danger] = "You have entered an empty search string"
     else
       @users = User.search(params[:search_param])
+      @users = current_user.except_current_user(@users)
       flash.now[:danger] = "No users match this search criteria" if @users.blank?
     end
     respond_to do |format|
       format.js { render partial: 'friends/result' }
     end
+    def add_friend
+      @friend = User.find(params[:friend])
+      current_user.friendships.build(friend_id: @friend.id)
+
+    if current_user.save
+      flash[:success] = "Friend was successfully added"
+    else
+      flash[:danger] = "There was something wrong with the friend request"
+    end
+    redirect_to my_friends_path
+    end
+
   end
 end
